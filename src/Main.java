089750b62e3297cf034e6b14959b6a61c4061d74
Александр <a@kistruga.ru>
2022-06-01 16:21:07 +0400

    ДЗ №7 Строки Готово добавил метод contains() с целью проверки на наличие буквы "ё" лобавил метод split(), такие требования)

--- src/Main.java
+++ src/Main.java
@@ -21,9 +21,14 @@ public class Main {
     }
     public static void task3() {
         String fullName1 = "Иванов Семён Семёнович.";
+        String fullName2 = null;
         if (fullName1.contains("ё")){
-        String fullName2 = fullName1.replace('ё', 'е');
-        System.out.println("Данные ФИО сотрудника - " + fullName2);
-    }
+             fullName2 = fullName1.replace('ё', 'е');
+        }
+        System.out.println("Данные ФИО сотрудника");
+        String[] fullName3 = fullName2.split(" ");
+        for (int i = 0; i < fullName3.length; i++) {
+            System.out.println(" - " + fullName3[i]);
+        }
     }
 }
